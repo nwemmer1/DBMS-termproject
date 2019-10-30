aecf6faa078b015c1a51ef1b9b5e50eb56c18d8c
Jacob Hanchett <jjh132@zips.uakron.edu>
2019-10-29 20:33:22 -0400

    start css

--- index.php
+++ index.php
@@ -8,24 +8,46 @@
         <link rel="stylesheet" href="css/styles.css">
         <link rel="author" href="humans.txt">
     </head>
+    <style type="text/css">
+        body{
+            text-align: center;
+            background-color: #336EA1;
+        }
+        h1{
+            font-size: 5em;
+            color: white;
+            margin-bottom: 0px;
+        }
+        h2{
+            margin-top: 5px;
+            font-size: 2.5em;
+            color: white;
+        }
+        img{
+            margin-bottom: 30px;
+        }
+        button{
+            font-size: 1.05em;
+        }
+    </style>
     <body>
-    	<h1>Welcome to the staff page!</h1>
+    	<h1>University of Akron</h1>
+        <h2><span style="color: #FFD61A;">STEM</span> Faculty Directory</h2>
+
+        <img style="height: 500px" src="images/Akron_seal.png">
+        <br>
+
     	<a href="add_entry.php"><button>Add a Faculty Member</button></a>
-        <br>
-        <br>
         <a href="searchOffice_form.php"><button>Search by room number</button></a>
-        <br>
-        <br>
         <a href="searchName_form.php"><button>Search by Last Name</button></a>
-        <br>
-        <br>
         <a href="searchDepartment_form.php"><button>Display Department Information</button></a>
-        <br>
-        <br>
         <a href="displayScholarships_form.php"><button>Search for Scholarships</button></a>
 
-
-
+        <br>
+        <br>
+        <footer>
+            Nathan Wemmer Jacob Hanchett
+        </footer>
 
     </body>
 </html>
