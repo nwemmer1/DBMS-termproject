9258546bf9439aa128ed1c1a0967ec317adf6f6c
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-16 14:44:03 -0500

    Add sql scripts for testing on different machines

---
+++ sql scripts/departments_data.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: select * from demo.departments
+LIMIT 0, 1000
+
+-- Date: 2019-11-16 14:41
+*/
+INSERT INTO `` (`department_id`,`department_name`,`building`,`numOfEmployees`) VALUES (1,'Computer Science','CAS',10);
+INSERT INTO `` (`department_id`,`department_name`,`building`,`numOfEmployees`) VALUES (2,'Mathematics','CAS',33);
+INSERT INTO `` (`department_id`,`department_name`,`building`,`numOfEmployees`) VALUES (3,'University of Akron','Buchtel',1963);
