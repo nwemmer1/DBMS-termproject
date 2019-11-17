6707a6a064b437d845726ff119a7ae8a7d0677d7
nwemmer1 <35506038+nwemmer1@users.noreply.github.com>
2019-11-16 23:39:32 -0500

    Add offices data with office number location

---
+++ sql scripts/office_data.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: select * from demo.offices
+LIMIT 0, 1000
+
+-- Date: 2019-11-16 23:36
+*/
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (10,'Mathematics','CAS  278','images/CAS 278.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (7,'Computer Science','CAS 221A','images/CAS 221A.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (3,'Computer Science','CAS 227','images/CAS 227.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (2,'Computer Science','CAS 228','images/CAS 228.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (1,'Computer Science','CAS 229','images/CAS 229.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (9,'Computer Science','CAS 233','images/CAS 233.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (5,'Computer Science','CAS 238','images/CAS 238.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (4,'Computer Science','CAS 239','images/CAS 239.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (6,'Computer Science','CAS 243','images/CAS 243.jpg','CAS');
+INSERT INTO `` (`employee_id`,`department`,`office_number`,`room_location`,`building`) VALUES (8,'Computer Science','CAS 244','images/CAS 244.jpg','CAS');
